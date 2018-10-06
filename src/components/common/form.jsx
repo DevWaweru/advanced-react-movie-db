dab82a470dd3d55636a76b602e4dcc18025a7822
Richard Waweru <developer.waweru@gmail.com>
2018-10-06 20:40:32 +0300

    - Refactor code for data abstraction by taking the input handler to form component

--- src/components/common/form.jsx
+++ src/components/common/form.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Joi from 'joi-browser';
+import Input from "./input";
 
 class Form extends Component {
     state = { 
@@ -41,6 +42,13 @@ class Form extends Component {
         const data = {...this.state.data}
         data[input.name] = input.value;
         this.setState({ data, errors })
+    }
+    renderButton(label){
+        return <button disabled={this.validate()} className="btn btn-outline-primary">{label}</button>
+    };
+    renderInput(name, label, type='text'){
+        const { data, errors } = this.state;
+        return <Input name={name} type={type} label={label} value={data[name]} onChange={this.handleChange} error={errors[name]} />
     }
 }
  
--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,6 +1,5 @@
-import React, { Component } from "react";
+import React from "react";
 import Joi from 'joi-browser';
-import Input from "./common/input";
 import Form from "./common/form";
 
 class LoginForm extends Form {
@@ -18,14 +17,13 @@ class LoginForm extends Form {
         console.log('submitted');
     }
     render() {
-        const { data, errors } = this.state;
         return (
         <div>
             <h1>Login</h1>
             <form onSubmit={this.handleSubmit}>
-                <Input name='username' label='Username' value={data.username} onChange={this.handleChange} error={errors.username} />
-                <Input name='password' label='Password' value={data.password} onChange={this.handleChange} error={errors.password} />
-                <button disabled={this.validate()} className="btn btn-outline-primary">Login</button>
+                {this.renderInput('username','Username')}
+                {this.renderInput('password','Password', 'password')}
+                {this.renderButton('Login')}
             </form>
         </div>
         );

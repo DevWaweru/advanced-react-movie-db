c845efa20461930ad34c2d65e3142ad138df8fda
Richard Waweru <developer.waweru@gmail.com>
2018-10-07 11:22:22 +0300

    - Add select component to render select inputs. It is a reusable component thus a good way for data abstraction - Add search Box component to render search input field. - Add input field in the Movie Form component from the form component. - Add render select function to render select input from the select component - Add filter functionality in the get paged data function in movies component to search for entries that have a specified keyword.

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from 'react-router-dom';
 import { getMovies } from "../services/fakeMovieService";
 import { getGenres } from "../services/fakeGenreService";
 import Pagination from "./common/pagination";
@@ -6,6 +7,7 @@ import { paginate } from "../utils/paginate";
 import ListGroup from "./common/listGroup";
 import MovieTable from "./movieTable";
 import _ from 'lodash';
+import SearchBox from "./searchBox";
 
 class FetchMovies extends Component {
   state = {
@@ -13,6 +15,8 @@ class FetchMovies extends Component {
     genres: [],
     currentPage: 1,
     pageSize: 4,
+    searchQuery: '',
+    selectedGenre: null,
     sortColumn:{path:'title', order:'asc'}
   };
   componentDidMount() {
@@ -38,15 +42,23 @@ class FetchMovies extends Component {
   };
   handleGenreSelect = genre => {
     // console.log(genre);
-    this.setState({ selectedGenre: genre, currentPage: 1 });
+    this.setState({ selectedGenre: genre, searchQuery:'', currentPage: 1 });
   };
   handleSort = (sortColumn) =>{
     // console.log(path);
     this.setState({sortColumn});
   }
+  handleSearch = (query) => {
+    this.setState({ searchQuery: query, selectedGenre: null, currentPage: 1 })
+  }
   getPagedData = () => {
-    const { currentPage, selectedGenre, pageSize, movies: allMovies, sortColumn } = this.state
-    const filtered = selectedGenre && selectedGenre._id ? allMovies.filter(m => m.genre._id === selectedGenre._id): allMovies;
+    const { currentPage, selectedGenre, pageSize, movies: allMovies, sortColumn, searchQuery } = this.state
+    let filtered = allMovies;
+    if (searchQuery)
+      filtered = allMovies.filter(m => m.title.toLowerCase().includes(searchQuery.toLowerCase()));
+    else if (selectedGenre && selectedGenre._id)
+      filtered = allMovies.filter(m => m.genre._id === selectedGenre._id);
+
     const sorted = _.orderBy(filtered, [sortColumn.path], [sortColumn.order])
     const movies = paginate(sorted, currentPage, pageSize);
 
@@ -54,7 +66,7 @@ class FetchMovies extends Component {
   }
   render() {
     const { length: count } = this.state.movies;
-    const { currentPage, pageSize, sortColumn } = this.state;
+    const { currentPage, pageSize, sortColumn, searchQuery } = this.state;
     if (count === 0) return <p className="text-center">There are no movies in the database</p>;
 
     const { totalCount, data: movies } = this.getPagedData();
@@ -69,6 +81,8 @@ class FetchMovies extends Component {
               />
             </div>
             <div className="col">
+              <Link to='/movies/new' className='btn btn-primary mb-2'>New Movie</Link>
+              <SearchBox value={searchQuery} onChange={this.handleSearch} />
               <p className="mt-4">Showing {totalCount} movies in the database </p>
               <MovieTable movies={movies} onDelete={this.handleDelete} onLike={this.handleLike} onSort={this.handleSort} sortColumn={sortColumn}/>
               <Pagination itemsCount={totalCount} pageSize={pageSize} onPageChange={this.handlePageChange} currentPage={currentPage} />
